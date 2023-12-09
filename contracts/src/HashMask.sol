cbd1836a89a0c856138ea627f633680b89ce2ba2
richuak <richuakuttikattu@gmail.com>
2023-12-09 13:51:05 +0300

    skeletal

--- contracts/src/HashMask.sol
+++ contracts/src/HashMask.sol
@@ -29,4 +29,41 @@
 
 pragma solidity ^0.8.20;
 
-contract HashMask {}
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+// import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+
+contract HashMask is FunctionsClient, ERC721 {
+    using FunctionsRequest for FunctionsRequest.Request;
+
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+
+    error UnexpectedRequestID(bytes32 requestId);
+
+    event Response(bytes32 indexed requestId, bytes response, bytes err);
+
+    constructor(address router, string memory name, string memory symbol)
+        FunctionsClient(router)
+        ERC721(name, symbol)
+    {}
+
+    function sendRequestCBOR(bytes memory request, uint64 subscriptionId, uint32 gasLimit, bytes32 donID)
+        external
+        returns (bytes32 requestId)
+    {
+        s_lastRequestId = _sendRequest(request, subscriptionId, gasLimit, donID);
+        return s_lastRequestId;
+    }
+
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+        if (s_lastRequestId != requestId) {
+            revert UnexpectedRequestID(requestId);
+        }
+        s_lastResponse = response;
+        s_lastError = err;
+        emit Response(requestId, s_lastResponse, s_lastError);
+    }
+}
